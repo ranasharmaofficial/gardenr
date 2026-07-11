eb74932e38f2a789e6143f408776de82f7e4e720
puja-03 <pujakumari11th2003@gmail.com>
2026-07-11 13:42:35 +0530

    setup project in local server so  path change in helpers.php  line no 102

--- app/Helpers.php
+++ app/Helpers.php
@@ -98,8 +98,8 @@
     if (!function_exists('static_asset')) {
         function static_asset($path, $secure = null)
         {
-            return app('url')->asset('public/' . $path, $secure);
-
+            // return app('url')->asset('public/' . $path, $secure);
+            return app('url')->asset($path, $secure);
         }
     }
 
